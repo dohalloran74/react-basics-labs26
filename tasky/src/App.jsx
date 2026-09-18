b0af6f505b2d44132e5d3caeab2e2794ba2129bd
Deirdre O'Halloran <Deirdre.OHalloran@setu.ie>
2026-09-18 09:23:57 +0100

    Tasky Lab 2a - Exercise done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -4,13 +4,14 @@ import React, { useState } from 'react';
 import './App.css';
 import Task from './components/Task';
 
+//added a new priority prop to the task component and passed it down form the app component.
 
 function App() {
   const [taskState, setTaskState] = useState({
     tasks: [
-      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" }
+      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", priority: "Low" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium" },
+      { id: 3, title: "Tidy up", description: "Clean the living room", deadline: "Today", priority: "High" }
     ]
   });
 
@@ -25,6 +26,8 @@ function App() {
           title={task.title}
           description={task.description}
           deadline={task.deadline}
+          // task.priority is passed down as a prop to the Task component, allowing it to be displayed in the task card.
+          priority={task.priority}
           key={task.id}
         />
       ))}
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -7,6 +7,8 @@ const Task = (props) => {
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className="description">{props.description}</p>
+            // The priority prop is displayed in the task card, allowing users to see the priority level of each task.
+            <p>Priority: {props.priority}</p>
             
         </div>
     )
