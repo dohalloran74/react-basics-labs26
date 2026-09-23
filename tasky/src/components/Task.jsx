6cde8e3a9db83eac1edc68db8191df48d635582e
Deirdre O'Halloran <Deirdre.OHalloran@setu.ie>
2026-09-23 15:02:24 +0100

    Style tasks and data and cards

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,18 +1,97 @@
 import React from 'react';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
   // The priority prop is displayed in the task card, allowing users to see the priority level of each task.
 const Task = (props) => {
     
     return (
-       <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
+       <Grid
+  key={props.id}
+  size={{ xs: 12, md: 4 }}
+>
+  <Card
+    sx={{
+      backgroundColor: props.done ? 'lightgrey' : 'lightblue',
+      padding: '20px'
+    }}
+  >
+    <CardHeader
+      title={props.title}
+      sx={{
+        backgroundColor: 'white',
+        borderRadius: '3px',
+        padding: '20px',
+        textAlign: 'center'
+      }}
+    />
 
+    <CardContent>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'baseline',
+          mb: 2,
+          padding: '20px'
+        }}
+      >
+        <Typography
+          component="p"
+          variant="subtitle2"
+          color="text.primary"
+        >
+          Due: {props.deadline}
+        </Typography>
+      </Box>
 
-            <p className="title">{props.title}</p>
-            <p>Due: {props.deadline}</p>
-            <p className="description">{props.description}</p>
-          
-          
+      <Typography
+        component="p"
+        variant="subtitle1"
+        align="center"
+        sx={{ fontStyle: 'italic' }}
+      >
+        {props.description}
+      </Typography>
+    </CardContent>
 
-            <p
+    <CardActions
+      sx={{
+        justifyContent: 'space-between',
+        padding: '20px'
+      }}
+    >
+      <Button
+        variant="contained"
+        size="small"
+        color="success"
+        onClick={props.markDone}
+      >
+        Done
+      </Button>
+
+      <Button
+        variant="contained"
+        size="small"
+        color="error"
+        onClick={props.deleteTask}
+      >
+        Delete
+      </Button>
+    </CardActions>
+  </Card>
+</Grid>
+
+    )
+}
+ {/*<p
         style={{
           backgroundColor:
             props.priority === "High"
@@ -25,20 +104,6 @@ const Task = (props) => {
         }}
       >
         {props.priority}
-      </p>
-
-         <button onClick={props.markDone} className='doneButton'>Done</button>
-
-                <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
-
-
-
-            
-
-
-
-        </div>
-    )
-}
+      </p> */}
 
 export default Task;
