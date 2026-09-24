b23b5ce49508cb0f3611fb4f2f9df060bc7e27a2
Deirdre O'Halloran <Deirdre.OHalloran@setu.ie>
2026-09-24 09:18:09 +0100

    Tasky Lab 3 done

--- tasky/src/components/Form.jsx
+++ tasky/src/components/Form.jsx
@@ -1,49 +1,71 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+
+
+
 const AddTaskForm = (props) => {
 
   return (
     <div>
-      <form onSubmit={props.submit}>
+      <Box
+  component="form"
+  sx={{
+    '& .MuiOutlinedInput-root': { m: 1, width: '30ch' },
+  }}
+  onSubmit={props.submit}
+>
+  <div>
+    <TextField
+      required
+      id="outlined-required"
+      name="title"
+      label="Task Title"
+      slotProps={{ inputLabel: { shrink: true } }}
+      onChange={(event) => props.change(event)}
+    />
+  </div>
 
-        <label>
-          Task title:
-          <input type="text" name="title" required onChange={(event) => props.change(event)} />
-        </label>
-        <br />
-        <label>
-          Due date:
-          <input type="date" name="deadline" required onChange={(event) => props.change(event)} />
-        </label>
-        <br />
-        <label>
-          Details:
-          <input type="text" name="description" onChange={(event) => props.change(event)} />
-        </label>
+  <div>
+    <TextField
+      required
+      name="deadline"
+      label="Deadline"
+      slotProps={{ inputLabel: { shrink: true } }}
+      type="date"
+      onChange={(event) => props.change(event)}
+    />
+  </div>
 
-        {/*Add in new priortiy field */}
+  <div>
+    <TextField
+      name="description"
+      id="outlined-multiline-static"
+      label="Task Details"
+      slotProps={{ inputLabel: { shrink: true } }}
+      multiline
+      rows={4}
+      onChange={(event) => props.change(event)}
+    />
+  </div>
 
-        <label>
-          Priority:
-          <select name="priority" onChange={(event) => props.change(event)}>
-            <option  value="Low">
-              Low{" "}
-            </option>
+  <div>
+    <Button
+      type="submit"
+      variant="contained"
+      color="primary"
+      sx={{
+        m: 1,
+        p: 1,
+        width: '95%'
+      }}
+    >
+      Add Task
+    </Button>
+  </div>
+</Box>
 
-            <option value="Medium">
-
-              Medium{" "}
-            </option>
-
-            <option value="High">
-
-              High{" "}
-            </option>
-
-          </select>
-        </label>
-
-
-        <input type="submit" value="Submit" />
-      </form>
     </div>
   )
 };
